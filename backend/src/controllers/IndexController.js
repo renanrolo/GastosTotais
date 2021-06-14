f875ad8bea250a37d52c6643c917d666cd7a0f52
Renan Rolo <renanrolo@hotmail.com>
2021-06-13 21:09:42 -0400

    removed backend folder

--- backend/src/controllers/IndexController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    async index(request, response) {
-        const helloWorld = {
-            Message: "Hello World!"
-        }
-        return response.json(helloWorld);
-    },
-}
--- backend/src/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, lowercase: true, unique: true, },
-    password: { type: String, min: 6, max: 12, required: true }
-});
-
-module.exports = mongoose.model('user', userSchema)
